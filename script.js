d39d88eb0f5acdd971a573b52581d83ff93a60f0
lazarev_sa <serg_lazarev13@mail.ru>
2025-01-26 23:50:51 +0400

    style chg

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ];
     //console.log(cards)
-    cards = getRandomUniqueCards(cards, 8)
+    cards = getRandomUniqueCards(cards, 2)
     cards = cards.flatMap(item => [item, item]);
     //console.log(cards)
 
